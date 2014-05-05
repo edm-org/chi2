9330b35d3e562cabbb99c542392495270640f639
Tom Arbesser <github@arbesser.org>
2014-05-05 16:20:55 +1000

    Minor progress on Chi2 - added 'increment'

--- client/1.0/Chi2.php
+++ client/1.0/Chi2.php
@@ -1,188 +1,236 @@
 <?php
 
-/*
+/**
  * Chi2 is a php library for storing and retrieving pre-aggregated statistics in Redis.
  * It consists of two major parts, the client library for directly interacting with the statistics in Redis,
  * and the REST-like server for retrieving bulk data in csv or json.
  * Detailed documentation on EDM Wiki.
  */
+class Chi2
+{
+	public static $hkey;
+	public static $add;
+	public static $increment;
+	public static $stats;
 
+	/**
+	 * Default options array
+	 * Options to be passed by default, like Redis' connection details (host and port) and System calling Chi2
+	 *
+	 * @var array
+	 */
+	private static $_options
+		= array(
+			'host'   => '127.0.0.1',
+			'port'   => 6379, // Redis default port: 6379
+			'system' => 'search-site-management',
+		);
 
-/**
- * Class Chi2
- */
-class Chi2 {
+	/**
+	 * Property that will hold the Redis object
+	 *
+	 * @var \Redis _connection
+	 */
+	private static $_connection = null;
 
-  /**
-  * Default options array
-  * Options to be passed by default, like Redi's connection details (Host, Port) and System calling the API
-  * @var array
-  */
-  private static $__options = array(
-    'host'  => "127.0.0.1",
-    'port'  => 6379, //Redis default port: 6379
-    'system'  =>  'search-site-management',
-  );
+	private function __construct()
+	{
+		// Can't construct
+	}
 
-  /**
-   * Property that will hold the Redis object
-   * @var __connection
-   */
-  private static $__connection;
+	/**
+	 * Connects to Redis using an existing Redis object $redis
+	 * or tries to create a new Redis object reading the this->_options
+	 *
+	 * @param \Redis|null $redis Redis object
+	 *
+	 * @throws Exception if can't connect to redis
+	 * @return boolean True if connected with Redis
+	 */
+	public static function setConnection(\Redis $redis = null)
+	{
+		if (is_null($redis)) {
+			$redis = new \Redis();
+			$result = $redis->connect(
+				self::$_options['host'],
+				self::$_options['port']
+			);
 
-  /**
-   * @var __makeKey
-   */
-  private static $__makeKey;
+			self::$_connection =& $redis;
 
-  /**
-   * @var __roundedTime
-   */
-  private static $__roundedTime;
+			return $result;
+		}
 
-  /**
-   *
-   * @var __token
-   */
-  private static $__token;
+		self::$_connection = $redis;
+		return true;
+	}
 
-  public static $hkey;
-  public static $add;
-  public static $increment;
-  public static $stats;
+	public static function ensureConnection()
+	{
+		if (is_null(self::$_connection)) {
+			self::setConnection();
+		}
+	}
 
-  /**
-   * Connects to Redis using an existing Redis object $redis
-   * or tries to create a new Redis object reading the this->__options
-   * @param $redis Redis object
-   * @throws Exception if can't connect to redis
-   * @return boolean True if connected with Redis
-   */
-  public static function setConnection(\Redis $redis=null){
-    // If $redis is sent then is assigned to $__connection
+	/**
+	 * Merge the passed options in the current options array
+	 *
+	 * @param array $options
+	 *
+	 */
+	public static function setOptions(array $options)
+	{
+		self::$_options = array_merge(self::$_options, $options);
+	}
 
-    // If it doesn't, tries to create the connection with Redis by using $this->__options
-  }
+	/**
+	 * Chi2 interacts directly with a Redis Server to store stats of many varieties,
+	 * Function increment(), increments a statistic given the provided parameters
+	 *
+	 * @param string $keyType The type of statistic being stored. Multikeys should be stored using dot notation.
+	 * @param string $key The key to store against, multiple keys should be merged with dot notation. eg: 'sg.ph'
+	 * @param string $statistic The name of the statistic you want to store. eg: 'clicks'
+	 * @param int|float  $inc
+	 * @param int|null $timestamp
+	 * @param int  $granularity
+	 * @param string|null $system The system calling Chi2. eg: 'search-site-management'
+	 *
+	 * @return float The new value
+	 */
+	public static function increment($keyType, $key, $statistic, $inc = 1, $timestamp = null, $granularity = 60, $system = null)
+	{
+		self::ensureConnection();
 
-  /**
-   * Merge the passed options in the current options array
-   * @param array $options
-   *
-   */
-  public static function setOptions(array $options)
-  {
-    //array_merge self::$__options and $options
-  }
+		$timestamp = self::_roundTime($timestamp, $granularity);
 
-  /**
-   * Chi2 interacts directly with a Redis Server to store stats of many varieties,
-   * Function increment(), increments a statistic given the provided parameters
-   *
-   * @param   $system     The system calling Chi2. eg: 'search-site-management'
-   * @param   $keyType    The type of statistic being stored. Multikeys should be stored using dot notation.
-   * @param   $key        The key to store against, multiple keys should be merged with dot notation. eg: 'sg.ph'
-   * @param   $statistic  The name of the statistic you want to store. eg: 'clicks'
-   * @param   $timestamp
-   * @param   int $inc
-   * @param   int $ttl
-   * @return  boolean     Returns True if it's a new field in the Redis hash and value was set,
-   */
-  public static function increment($system, $keyType, $key, $statistic, $timestamp, $inc=1, $ttl=2592000)
-  {
+		$hashKey = self::_makeKey($keyType, $key, $statistic, $system);
 
-    // Before doing anything else check that the Redis connection is present
+		return self::$_connection->hIncrByFloat($hashKey, $timestamp, $inc);
+	}
 
-    // Round the hour __roundTime()
+	/**
+	 * Chi2 allows retrieve statistics on demand. It gets a statistic array (timestamps) based on provided parameters
+	 * and the date range, using $grain to determine the granularity returned
+	 *
+	 * @param       $system       System that requests the stats
+	 * @param       $keyType      The aggregation key types  Eg. $keyTypes = array('feed_id', 'subid')
+	 * @param       $key          The aggregation values
+	 * @param       $statistic    The kind of statistic that will be queried
+	 * @param       $time_start   Starting point (From)
+	 * @param       $time_end     Ending point (To)
+	 * @param   int $granularity  Used to determine the granularity returned
+	 *
+	 * @return  array         return stats results, depending on the date range and granularity specified in the query
+	 */
+	public static function getStatistic($keyType, $key, $statistic, $time_start, $time_end = null, $granularity = 60, $system = null)
+	{
+		self::ensureConnection();
 
-    // Get hashKey created by __makeKey()
+		if (is_null($system)) {
+			$system = self::$_options['system'];
+		}
 
-    // Add the values to the hashKey in Redis.
+		//self::_roundTimespan($)
 
-    //$increment = self::$__connection->hset($hkey, $timestamp, $inc);
+		// Get hashKey created by _makeKey()
 
-  }
+		// Check if the hashKey exists in Redis.
 
-  /**
-   * Generate the hashKey to be used when interacting with the stats (create or query)
-   *
-   * @param $system
-   * @param $keyType
-   * @param $key
-   * @param $statistic
-   * @param $timestamp
-   * @return string
-   */
-  private static function __makeKey($system, $keyType, $key, $statistic, $timestamp){
-    // Generate hashKey
-    return;
-  }
+		// Get values from Redis
 
-  /**
-   * Use to round any time day/hour/minute
-   * @param $grain int
-   * @param $time_start int
-   * * @param $time_end int
-   * @return  int[]
-   */
-  private static function __roundTime($grain=null, $time_end=null, $time_start) {
-    //If the $grain and the $time_end are given, round the time to get the range
+	}
 
-    // Else Round to hour by default, if no $grain is given
-  }
-  /**
-   * Chi2 allows retrieve statistics on demand. It gets a statistic array (timestamps) based on provided parameters
-   * and the date range, using $grain to determine the granularity returned
-   *
-   * @param   $system       System that requests the stats
-   * @param   $keyType      The aggregation key types  Eg. $keyTypes = array('feed_id', 'subid')
-   * @param   $key          The aggregation values
-   * @param   $statistic    The kind of statistic that will be queried
-   * @param   $time_start   Starting point (From)
-   * @param   $time_end     Ending point (To)
-   * @param   int $grain    Used to determine the granularity returned
-   * @return  array         return stats results, depending on the date range and granularity specified in the query
-   */
-  public static function getStatistic($system, $keyType, $key, $statistic, $time_start, $time_end, $grain=60)
-  {
-    // Check Redis $connection if not present, try to connect
+	/**
+	 * Generate the hashKey to be used when interacting with the stats (create or query)
+	 *
+	 * @param $system
+	 * @param $keyType
+	 * @param $key
+	 * @param $statistic
+	 *
+	 * @return string
+	 */
+	private static function _makeKey($keyType, $key, $statistic, $system = null)
+	{
+		if (is_null($system)) {
+			$system = self::$_options['system'];
+		}
 
-    // Round the hour __roundTime()
+		return 'stats:' . $system . ':' . $keyType . ':' . $key . ':' . $statistic;
+	}
 
-    // Get hashKey created by __makeKey()
+	/**
+	 * Use to round any time day/hour/minute
+	 *
+	 * @param $timestamp int
+	 * @param $granularity int
+	 *
+	 * @return int
+	 */
+	private static function _roundTime($timestamp, $granularity)
+	{
+		if (is_null($timestamp)) {
+			$timestamp = time();
+		}
 
-    // Check if the hashKey exists in Redis.
+		$grain = floor($timestamp / $granularity);
+		$timestamp = $grain * $granularity;
 
-    // Get values from Redis
+		return $timestamp;
+	}
 
-  }
+	private static function _getTimeSlices($startTime, $endTime, $granularity)
+	{
+		$timestamps = array();
 
-  /**
-   * All queries to Chi2 must be accompanied by a X-token header
-   * Set the authentication token, which shall be calculated as follows
-   * $token = md5( $system . floor( time() / 3600 ) * 3600);
-   * @param $system string The name of the system calling Chi2
-   */
-  private static function __setToken($system)
-  {
-    //$this->__token = md5( $system . floor( time() / 3600 ) * 3600);
-  }
+		if (is_null($startTime)) {
+			$startTime = time();
+		}
 
-  /**
-   * Read the X-token header sent by the browser
-   * @param $system
-   */
-  private static function __getToken($system){
+		$grain = floor($startTime / $granularity);
+		$startTime = $grain * $granularity;
 
-  }
+		$time = $startTime;
+		while (($time + $granularity) <= $endTime) {
+			$timestamps[] = $time;
+		}
 
-  /**
-   * Validates token based on the name of the system and the $token
-   * @param $system
-   * @param $token\
-   * @return boolean Returns true if the token matches with the one in the System.
-   */
-  private static function __verifyToken($system, $token){
+		return $timestamps;
+	}
 
-  }
+	/**
+	 * All queries to Chi2 must be accompanied by a X-token header
+	 * Set the authentication token, which shall be calculated as follows
+	 * $token = md5( $system . floor( time() / 3600 ) * 3600);
+	 *
+	 * @param $system string The name of the system calling Chi2
+	 */
+	private static function _setToken($system)
+	{
+		//$this->_token = md5( $system . floor( time() / 3600 ) * 3600);
+	}
+
+	/**
+	 * Read the X-token header sent by the browser
+	 *
+	 * @param $system
+	 */
+	private static function _getToken($system)
+	{
+
+	}
+
+	/**
+	 * Validates token based on the name of the system and the $token
+	 *
+	 * @param $system
+	 * @param $token\
+	 *
+	 * @return boolean Returns true if the token matches with the one in the System.
+	 */
+	private static function _verifyToken($system, $token)
+	{
+
+	}
 
 }
